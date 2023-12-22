3a01c8a87d1faeb9c56fbcdbdf3611b3738e7969
Balogh Dávid <david.balogh.2002@gmail.com>
2023-12-22 17:12:21 +0100

    skillek

--- src/components/helpers/skillsList.jsx
+++ src/components/helpers/skillsList.jsx
@@ -1,25 +1,34 @@
 export const items = [
-    { 
-      id: 1, 
-      image:require("../../assets/java.png"),
-    },
-    { 
-      id: 2,
-      image:require("../../assets/java.png"),
-      },
-    { 
-      id: 3, image:require("../../assets/java.png"),
-    },
-    { 
-      id: 4, 
-      image:require("../../assets/java.png"),
+    {
+        id: 1,
+        image:require("../../assets/skills/reactjs.png"),
     },
     {
-       id: 5, 
-       image:require("../../assets/java.png"),
-      },
-    { 
-      id: 6, 
-      image:require("../../assets/java.png"),
+        id: 2,
+        image:require("../../assets/skills/angular.png"),
+    },
+    {
+        id: 3,
+        image:require("../../assets/skills/spring.png"),
+    },
+    {
+        id: 4,
+        image:require("../../assets/skills/mongodb.png"),
+    },
+    {
+        id: 5,
+        image:require("../../assets/skills/mysql.png"),
+    },
+    {
+        id: 6,
+        image:require("../../assets/skills/kafka.png"),
+    },
+    {
+        id: 7,
+        image:require("../../assets/skills/git.png"),
+    },
+    {
+        id: 8,
+        image:require("../../assets/skills/linux.png"),
     },
   ];
